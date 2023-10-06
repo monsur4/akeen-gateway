06574f687cfef40f9ac981663e553e1aa75aee81
monsur4 <31864071+monsur4@users.noreply.github.com>
2023-10-06 17:15:42 +0100

    Create TokenValidationServiceImpl.java

---
+++ src/main/java/com/mon/akeengateway/validation/impl/TokenValidationServiceImpl.java
@@ -0,0 +1,22 @@
+package com.mon.akeengateway.validation.impl;
+
+@Service
+@Slf4j
+public class TokenValidationServiceImpl implements TokenValidationService {
+
+    @Override
+    public boolean isValidToken(String token){
+        ValidateTokenRequest request = ValidateTokenRequest.builder()
+                .setToken(token)
+                .build();
+
+        log.info("calling authentication service with validateTokenRequest: [{}]", request);
+
+        // TODO: MAKE CALL TO AUTHENTICATION SERVICE
+
+        ValidateTokenResponse validateTokenResponse = new ValidateTokenResponse();
+        log.info(validateTokenResponse.toString());
+
+        return validateTokenResponse.getIsValid();
+    }
+}
